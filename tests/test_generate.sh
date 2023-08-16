a1fb37289833d9f5bfd0e49c77feb72782aad274
Ilya Bushmakin <im.bushmakin@gmail.com>
2023-08-16 19:28:25 +0400

    refactor: change git user

--- tests/test_generate.sh
+++ tests/test_generate.sh
@@ -13,7 +13,7 @@ echo
 generate "${PWD}" "${output}"
 cd "${output}"
 git init .
-git remote add origin https://github.com/pawamoy/pawamoy-testing
+git remote add origin https://github.com/ilbumi/ilbumi-testing
 
 echo
 echo "///////////////////////////////////////////"
