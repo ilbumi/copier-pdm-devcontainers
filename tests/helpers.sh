cdc86e8e9a085866449344d09373e30ba9fe2e63
Ilya Bushmakin <im.bushmakin@gmail.com>
2023-08-16 19:52:40 +0400

    fix: :ambulance: fix unexpected EOF  in tests
    fixed unexpected EOF in tests/helpers.sh

--- tests/helpers.sh
+++ tests/helpers.sh
@@ -5,5 +5,5 @@ generate() {
     -d project_description='Testing this great template' \
     -d author_fullname="Ilya Bushmakin" \
     -d author_username="ilbumi" \
-    -d author_email="i.bushmakin@gmail.com" \
+    -d author_email="i.bushmakin@gmail.com"
 }
